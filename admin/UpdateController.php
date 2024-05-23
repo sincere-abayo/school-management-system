8919b9d4aab1bf9e393fde545599901fe501bdfc
sincere-abayo <abayosincere11@gmail.com>
2024-05-23 13:27:43 +0200

    fyhg

--- admin/UpdateController.php
+++ admin/UpdateController.php
@@ -4,26 +4,20 @@
 include '../SessionController.php';
 include '../config.php';
 
-if (!isset($_POST['school_id'])) {
+if (!isset($_POST['school-id'])) {
    echo "only post method allowed";
 }
 
 else {    
 $school_name=$_POST['school-name'];
-$district=$_POST['district'];
-$sector=$_POST['sector'];
-$phone=$_POST['phone'];
-$email=$_POST['email'];
-$user=$_POST['user'];
-$password=$_POST['password'];
-$id=$_POST['school_id'];
-$updateQuery= "UPDATE schools set School_name='$school_name',District='$district',Sector='$sector',Phone='$phone',Email='$email' where school_id='$id'";
+$id=$_POST['school-id'];
+$updateQuery= "UPDATE schools set School_name='$school_name' where school_id='$id'";
 // Prepare and execute statement
 $stmt = $conn->query($updateQuery);
 
 // Execute the statement
 if ($stmt) {
-    echo "School inserted successfully.";
+    echo "School updated successfully.";
 }
  else {
     echo "Error: " . $conn->error;
